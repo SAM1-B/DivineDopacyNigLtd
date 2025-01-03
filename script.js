ae8eef4bb10de87b1241e74d5b455e982ecb9277
samuel <babatundeola876@gmail.com>
2025-01-03 02:17:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// window.addEventListener("scroll", function(){
+//     var header= document.getElementsByClassNamer("header");
+//     header.classList.toggle("sticky", this.window.scrollY > 0);
+// })
